124f6afb4ca85c2ad2c11b41382582660b5cdcb1
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2021-08-10 13:16:37 -0700

    Update web.php

--- MSC/routes/web.php
+++ MSC/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,6 +25,20 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+//admin get slot
+Route::get('/sunday', function(Request $request){
+    return DB::table('sundays')->get();
+ });
+
+//admin create slots
+Route::post('/sunday', function(Request $request){
+    DB::table('sundays')->insert([
+        'title' =>$request->input('title'),
+        'date' =>$request->input('date'),
+        'slot' =>$request->input('slot'),
+        'created_at'=> Carbon::now()    
+    ]);
+ });
 
 //admin create slots
 Route::post('/sunday', function(Request $request){
@@ -29,7 +46,37 @@ Route::post('/sunday', function(Request $request){
        'title' =>$request->input('title'),
        'date' =>$request->input('date'),
        'slot' =>$request->input('slot'),
+       'created_at'=> Carbon::now()    
    ]);
-})
+});
+
+//admin update slots
+Route::post('/sunday', function(Request $request){
+    DB::table('sundays')->update([
+        'title' =>$request->input('title'),
+        'date' =>$request->input('date'),
+        'slot' =>$request->input('slot'),
+        'created_at'=> Carbon::now() 
+    ]);
+ });
+
+ //admin delete slots
+Route::get('/sunday/delete/{id}', function($id){
+    DB::table('sundays')->where('id',$id)->delete();
+ });
 
 
+ //user booking
+ //attend booking
+Route::post('/attend', function(Request $request){
+    DB::table('attendees')->insert([
+        'user_id' =>Auth::user()->id,
+        'sunday_id' =>$request->input('sunday_id'),
+        'created_at'=> Carbon::now()    
+    ]);
+ });
+
+//cancel booking
+Route::get('/attend/delete/{id}', function($id){
+    DB::table('attendees')->where('user_id',Auth::user()->id)->where('sunday_id',$id)->delete();
+ });
